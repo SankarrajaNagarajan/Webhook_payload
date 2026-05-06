a51c6313794bcb8ad08e377a99e2c86e4800882d
Sankarraja <sankarraja.ni@keenminds.in>
2026-05-06 23:41:04 +0530

    changes

---
+++ Dpp.Webhook.Domain/Entities/WebhookPayload.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dpp.Webhook.Domain.Entities;
+
+public class WebhookPayload
+{
+    public Guid Id { get; set; } = Guid.NewGuid();
+    //public DateTime DateReceived { get; set; } = DateTime.UtcNow.Date;
+    public string DateReceived { get; set; } 
+    public DateTime DateTimeReceived { get; set; } = DateTime.UtcNow;
+    public string Payload { get; set; }
+    public string Source { get; set; }
+}
